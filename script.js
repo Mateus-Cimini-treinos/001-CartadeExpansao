f595c8aefbab95d19755c6254055336fd74e2999
Mateus Cimini Araujo <mateusaraujo0123456789@gmail.com>
2024-07-01 13:07:33 -0300

    att
    esconde os links

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
+// const panels = document.querySelectorAll(".painel");
+
+//panels.forEach((painel) => {
+  //  painel.addEventListener("click", () => {
+      //  removeActiveClasses();
+       // painel.classList.add("atividade");
+    //});
+//});
+
+//const removeActiveClasses = () => {
+   // panels.forEach((painel) => {
+    //    painel.classList.remove("atividade");
+    //});
+//};
+
 const panels = document.querySelectorAll(".painel");
+const xinZhaoLink = document.querySelector(".painel:nth-child(2) a");
+const xinZhaoIcon = document.querySelector(".painel:nth-child(2) i");
 
 panels.forEach((painel) => {
     painel.addEventListener("click", () => {
         removeActiveClasses();
         painel.classList.add("atividade");
+
+        // Verifica se o painel clicado é o do Xin-Zhao
+        if (painel.classList.contains("atividade") && painel.querySelector("h3").textContent === "Xin-Zhao") {
+            xinZhaoLink.style.display = "inline"; // Mostra o link
+            xinZhaoIcon.style.display = "inline"; // Mostra o ícone
+        } else {
+            xinZhaoLink.style.display = "none"; // Esconde o link caso não seja o Xin-Zhao
+            xinZhaoIcon.style.display = "none"; // Esconde o ícone caso não seja o Xin-Zhao
+        }
     });
 });
 
@@ -12,3 +38,4 @@ const removeActiveClasses = () => {
         painel.classList.remove("atividade");
     });
 };
+
